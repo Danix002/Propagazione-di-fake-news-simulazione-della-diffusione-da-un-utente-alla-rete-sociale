b7d862ce9e712f400e49dd4c8c199296ad8170c5
PatricReineri <onepat96@gmail.com>
2024-05-29 16:08:00 +0200

    Added Threshold model for debunking

--- simulation_artificial_model.py
+++ simulation_artificial_model.py
@@ -1,60 +1,68 @@
 import matplotlib
 matplotlib.use('Qt5Agg')
 import networkx as nx
-import dynetx as dn
-import matplotlib.pyplot as plt
-from past.builtins import xrange
 import ndlib.models.ModelConfig as mc
-import ndlib.models.dynamic as dm
-import ndlib.models.epidemics as ep
-from ndlib.utils import multi_runs
-from ndlib.viz.mpl.DiffusionTrend import DiffusionTrend
-from ndlib.viz.bokeh.DiffusionPrevalence import DiffusionPrevalence
-from ndlib.viz.mpl.TrendComparison import DiffusionTrendComparison
-import random
 import ndlib.models.CompositeModel as gc
-import stochastic_attribute_model as sm
 import ndlib.models.compartments as cpm
+import matplotlib.pyplot as plt
+import random
+import math
+import ndlib.models.ModelConfig as mc
+import ndlib.models.epidemics as ep
+
+# Funzione di probabilità di infezione personalizzata usando una sigmoide
+def custom_infection_probability(node, graph):
+    # Esempio di calcolo usando una sigmoide degli attributi del nodo
+    attribute = graph.nodes[node]['percentage_of_knowledge']  # Usa uno degli attributi
+    sigmoid = 1 / (1 + math.exp(-attribute))
+    return sigmoid
 
 n = 2000 # Numero totale di nodi
 m = 3 # Numero di archi da aggiungere ad ogni nuovo nodo
 
-# Network Definition
+# Definizione del grafo
 g = nx.barabasi_albert_graph(n, m)
 
-# Add node attributes for infection risk
-credibility_of_fake_news = 0.4 # Parametro assegnato manualmente in base alla tipologia di fake-news con cui si vuole provare il processo di diffusione e sfatamento 
+# Aggiunta di attributi ai nodi
 for node in g.nodes():
     g.nodes[node]['percentage_of_knowledge'] = random.uniform(0.5, 1.5)
-    g.nodes[node]['probability_of_fat-checking'] = random.uniform(0.5, 1.5)
+    g.nodes[node]['probability_of_fact-checking'] = random.uniform(0.5, 1.5)
     g.nodes[node]['distance_from_the_center_of_diffusion'] = random.uniform(0.5, 1.5)
     g.nodes[node]['age'] = random.uniform(0.5, 1.5)
     g.nodes[node]['probability_of_forgetting'] = random.uniform(0.5, 1.5)
-    g.nodes[node]['infection_risk'] = random.uniform(0.5, 1.5)
 
-# Model Selection
+# Selezione del modello
 model = gc.CompositeModel(g)
 
-# Model statuses
+# Aggiunta degli stati del modello
 model.add_status("Susceptible")
 model.add_status("Infected")
-model.add_status("Removed")
+model.add_status("Recovered")
 
-c1 = cpm.NodeStochastic(0.02, triggering_status = "Infected")
-#c1 = sm.stochastic_attribute_model(0.1, attribute = "infection_risk", triggering_status = "Infected") 
-model.add_rule("Susceptible", "Infected", c1)
+
 config = mc.Configuration()
-config.add_model_parameter("fraction_infected", 0.2)
+config.add_model_parameter('fraction_infected', 0.2)
+print(config.get_model_parameters())
 
-# Simulation multiple execution
-#e_n = 4 # Execution number = to number of infection sets
-#i_n = 10 # Iteration Number - number of days
-#infection_sets = [(0, 1), (2, 3), (4, 5), (6, 7)]
+
+# Definizione delle regole di transizione usando compartimenti
+c1 = cpm.NodeStochastic(0.0, triggering_status="Infected")
+model.add_rule("Susceptible", "Infected", c1)
+
+c2 = cpm.NodeThreshold(0.5, triggering_status = "Infected")
+model.add_rule("Infected", "Recovered", c2)
+
+# Imposto la probabilità di infezione personalizzata per ogni nodo
+for i in g.nodes():
+    beta = custom_infection_probability(i, g)
+    config.add_node_configuration("ratio", i, beta)
+    config.add_node_configuration("threshold", i, 0.2)
 
 model.set_initial_status(config)
 
-# Simulation
-iterations = model.iteration_bunch(300)
+# Esecuzione della simulazione
+iterations = model.iteration_bunch(20)
+print(iterations)
 
 # Colori dei nodi in base al loro stato
 colors = ["blue"] * len(g.nodes)
@@ -63,12 +71,16 @@ for i in range(0, len(iterations)):
     for key in node_statuses.keys():
         if node_statuses[key] == 1:  # Stato infetto
             colors[key] = 'red'
+        elif node_statuses[key] == 2:
+            colors[key] = 'green'
         else:
             colors[key] = 'blue'
 
 # Add the 'color' attribute to the nodes
 for node, color in zip(g.nodes(), colors):
     g.nodes[node]['color'] = color
+
+
 
 # Show simulation
 #viz = DiffusionTrend(model, trends)
