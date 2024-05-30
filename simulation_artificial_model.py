a749e53e4b697049cb1fd4a47789c9b419e14464
PatricReineri <onepat96@gmail.com>
2024-05-30 09:34:07 +0200

    add new infected transition status  to iteration status of the model

--- simulation_artificial_model.py
+++ simulation_artificial_model.py
@@ -34,16 +34,22 @@ def custom_iteration(model, graph, current_iteration):
     actual_status = model.status.copy()
     new_status = actual_status.copy()
     count_new_infected = 0
+    dict_new_status = {}
     for node in graph.nodes():
         if actual_status[node] == 1:  # Nodo infetto
             for neighbor in graph.neighbors(node):
                 if actual_status[neighbor] == 0:  # Nodo suscettibile
-                    infection_risk =  custom_infection_probability(node, graph)
+                    infection_risk =  custom_infection_probability(neighbor, graph)
                     if random.random() < infection_risk:
                         new_status[neighbor] = 1  # Diventa infetto
+                        dict_new_status[neighbor] = 1
                         count_new_infected = count_new_infected + 1
     model.status = new_status
     current_iteration_results = model.iteration_bunch(1)[0]
+    
+    #add dict_new status to the current iteration status
+    for key in dict_new_status.keys():
+        current_iteration_results["status"][key] = dict_new_status[key]
     current_iteration_results["status_delta"][0] = count_new_infected
     return current_iteration_results
 
