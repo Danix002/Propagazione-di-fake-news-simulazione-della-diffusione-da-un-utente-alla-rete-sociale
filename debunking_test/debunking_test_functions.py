f04fe4d57402c94974a97cff97d21f90da08648e
PatricReineri <onepat96@gmail.com>
2024-06-01 20:03:35 +0200

    add debunking test functions and add test2

---
+++ debunking_test/debunking_test_functions.py
@@ -0,0 +1,115 @@
+import networkx as nx
+import dynetx as dn
+import matplotlib.pyplot as plt
+import math
+import custom_iterations_bunch as cib
+
+
+
+
+
+
+def print_test_results(name_test, iterations):
+    # visualize all iteration but merge iteration that have the same status
+    print("\n\n<---------TEST: ", name_test, "--------->\n")
+    
+    print("\nINITIAL STATUS AT ITERATION 0 - ", iterations[0]["node_count"])
+    for i  in range(1, len(iterations)):
+        node_statuses = iterations[i]["status"]
+        count_susceptible = 0
+        count_infected = 0
+        count_recovered = 0
+        for key in node_statuses.keys():
+            if node_statuses[key] == 1:
+                count_infected += 1
+            elif node_statuses[key] == 2:
+                count_recovered += 1
+            else:
+                count_susceptible += 1
+        # add 'node_count' in the print of the iteration
+        print("Iteration: ", i, "newInfected: ", count_infected, "newRecovered: ", count_recovered, "node count: ", iterations[i]["node_count"])
+        
+        # if status is the same of the previous iteration for 5 time, print "Stable status"
+        if i > 5:
+            if iterations[i]["status"] == iterations[i-1]["status"] == iterations[i-2]["status"] == iterations[i-3]["status"] == iterations[i-4]["status"]:
+                print("Stable status - end of the simulation at iteration ",i," of ", len(iterations))
+                break
+       
+def write_graph_to_file(g, file_name, iterations):
+    colors = ["blue"] * len(g.nodes)
+    for i in range(0, len(iterations)):
+        node_statuses = iterations[i]["status"]
+        for key in node_statuses.keys():
+            if node_statuses[key] == 1:  # State infected
+                colors[key] = 'red'
+            if node_statuses[key] == 2: # State recovered
+                colors[key] = 'green'
+                
+    # Add the 'color' attribute to the nodes
+    for node, color in zip(g.nodes(), colors):
+        g.nodes[node]['color'] = color
+        
+    #write the graph to a file
+    nx.write_graphml(g, file_name)
+
+
+def make_test_1_debunking(g, model, config, num_iterations, fake_news_credibility):
+    #-- DEBUNKING TEST 1: setting as initial recovered seed all the hub of the graph, using the degree centrality
+    intial_seed = []
+
+    # Get the degree centrality of the nodes
+    degree_centrality = nx.degree_centrality(g)
+
+    # Get the nodes (10%) with the highest degree centrality
+    num_nodes = g.number_of_nodes()
+    num_hub_nodes = math.ceil(num_nodes * 0.1)
+
+    hub_nodes = sorted(degree_centrality, key=degree_centrality.get, reverse=True)
+    sorted_hubs = hub_nodes[:num_hub_nodes]
+
+    # Add the hub nodes to the initial seed
+    for node in sorted_hubs:
+        intial_seed.append(node)
+
+    config.add_model_initial_configuration("Recovered", intial_seed)
+    model.set_initial_status(config)
+
+    # Esecution of the simulation
+    iterations = cib.custom_iteration_bunch(model, g, num_iterations, fake_news_credibility)
+
+    # Visualization of the simulation results
+    name_test = "Debunking test 1: setting as initial recovered seed all the hub of the graph"
+    print_test_results(name_test, iterations)
+    write_graph_to_file(g, "debunking_test_1_hubs.graphml", iterations)
+    
+    return iterations
+
+def make_test_2_debunking(g, model, config, num_iterations, fake_news_credibility):
+    #-- DEBUNKING TEST 2: setting the 20% with the highest betweennes centrality as initial seed
+    intial_seed = []
+    betweenness_centrality = nx.betweenness_centrality(g, normalized=True)
+
+    num_nodes = g.number_of_nodes()
+    num_bridging_nodes = math.ceil(num_nodes* 0.2)
+
+    sorted_nodes = sorted(betweenness_centrality, key=betweenness_centrality.get, reverse=True)
+    bridging_nodes = sorted_nodes[:num_bridging_nodes]
+
+    # Add the hub nodes to the initial seed
+    for node in bridging_nodes:
+        intial_seed.append(node)
+
+    config.add_model_initial_configuration("Recovered", intial_seed)
+    model.set_initial_status(config)
+
+    # Esecution of the simulation
+    iterations = cib.custom_iteration_bunch(model, g, num_iterations, fake_news_credibility)
+
+    # Visualization of the simulation results
+    name_test = "Debunking test 2: setting as initial recovered seed all the local bridges nodes"
+    print_test_results(name_test, iterations)
+    write_graph_to_file(g, "debunking_test_2_hubs.graphml", iterations)
+    
+    return iterations
+    
+    
--- simulation_artificial_model.py
+++ simulation_artificial_model.py
@@ -27,52 +27,12 @@ import attribute.instruction as ins
 import attribute.spatial_coordinate as sc
 import geopandas as gpd
 import artificial_network_generate as ang
+import custom_iterations_bunch as cib
+from debunking_test.debunking_test_functions import make_test_1_debunking, make_test_2_debunking
 
 
-def print_test_results(name_test, iterations):
-    # visualize all iteration but merge iteration that have the same status
     
-    print("\n\n<---------TEST: ", name_test, "--------->\n")
-    # print initial status
-    print("\nINITIAL STATUS AT ITERATION 0 - ", iterations[0]["node_count"])
-    for i  in range(1, len(iterations)):
-        node_statuses = iterations[i]["status"]
-        count_susceptible = 0
-        count_infected = 0
-        count_recovered = 0
-        for key in node_statuses.keys():
-            if node_statuses[key] == 1:
-                count_infected += 1
-            elif node_statuses[key] == 2:
-                count_recovered += 1
-            else:
-                count_susceptible += 1
-        # add 'node_count' in the print of the iteration
-        print("Iteration: ", i, "newInfected: ", count_infected, "newRecovered: ", count_recovered, "node count: ", iterations[i]["node_count"])
-        
-        # if status is the same of the previous iteration for 5 time, print "Stable status"
-        if i > 5:
-            if iterations[i]["status"] == iterations[i-1]["status"] == iterations[i-2]["status"] == iterations[i-3]["status"] == iterations[i-4]["status"]:
-                print("Stable status - end of the simulation at iteration ",i," of ", len(iterations))
-                break
-       
-def write_graph_to_file(g, file_name, iterations):
-    colors = ["blue"] * len(g.nodes)
-    for i in range(0, len(iterations)):
-        node_statuses = iterations[i]["status"]
-        for key in node_statuses.keys():
-            if node_statuses[key] == 1:  # State infected
-                colors[key] = 'red'
-            if node_statuses[key] == 2: # State recovered
-                colors[key] = 'green'
-                
-    # Add the 'color' attribute to the nodes
-    for node, color in zip(g.nodes(), colors):
-        g.nodes[node]['color'] = color
-        
-    #write the graph to a file
-    nx.write_graphml(g, file_name)
-
+    
 # 1) Creation of a Barabasi-Albert graph with Rank model extension
 n = 200 # Numero totale di nodi
 m = 3 # Numero di archi da aggiungere ad ogni nuovo nodo
@@ -105,7 +65,7 @@ model.add_status("Infected")
 model.add_status("Recovered")
 
 config = mc.Configuration()
-config.add_model_parameter('fraction_infected', 0.02)
+config.add_model_parameter('fraction_infected', 0.2)
 
 # Transition from Susceptible to Infected
 c1 = cpm.NodeStochastic(0, triggering_status = "Infected")
@@ -120,55 +80,23 @@ for i in g.nodes():
     config.add_node_configuration("threshold", i, cib.custom_infection_probability(i, g, fake_news_credibility)-0.1)
 
 # 3.2) Configuration of the simulation
-
 #-- DEBUNKING TEST 1: setting as initial recovered seed all the hub of the graph, using the degree centrality
+#iterations_test_one = make_test_1_debunking(g, model, config, 50, fake_news_credibility)
 
-intial_seed = []
-
-# Get the degree centrality of the nodes
-degree_centrality = nx.degree_centrality(g)
-
-# Get the nodes (10%) with the highest degree centrality
-num_hub_nodes = math.ceil(n * 0.1)
-
-hub_nodes = sorted(degree_centrality, key=degree_centrality.get, reverse=True)[:num_hub_nodes]
-
-# Add the hub nodes to the initial seed
-for node in hub_nodes:
-    intial_seed.append(node)
-    
-config.add_model_initial_configuration("Recovered", intial_seed)
-model.set_initial_status(config)
-
-# Esecution of the simulation
-iterations = cib.custom_iteration_bunch(model, g, 50, fake_news_credibility)
-
-# 4) Visualization of the simulation results
-name_test = "Debunking test 1: setting as initial recovered seed all the hub of the graph"
-print_test_results(name_test, iterations)
-write_graph_to_file(g, "debunking_test_1_hubs.graphml", iterations)
-# Show simulation
-trends = model.build_trends(iterations)
-
-#-- DEBUNKING TEST 2: 
+#-- DEBUNKING TEST 2: setting the 20% with the highes betweennes centrality as initial seed
+iterations_test_two = make_test_2_debunking(g, model, config, 50, fake_news_credibility)
 
 #-- DEBUNKING TEST 3: 
-#viz = DiffusionTrend(model, trends)
-#viz.plot()  # Visualizza il grafico con Matplotlib
-
-# Salva la figura
-#plt.savefig("diffusion_SIR.png")
 
 
-# Debunking (classificazione del punto di partenza)
 
 
 
 
 def get_infected_node():
     infected_node = []
-    for i  in range(1, len(iterations)):
-        node_statuses = iterations[i]["status"]
+    for i  in range(1, len(iterations_test_two)):
+        node_statuses = iterations_test_two[i]["status"]
         for key in node_statuses.keys():
             if node_statuses[key] == 1:  # State infected
               infected_node.append(g.nodes()[key])
