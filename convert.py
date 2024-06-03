3a52265d9000b30595bc8e3de6a6b06cd3684cd8
PatricReineri <onepat96@gmail.com>
2024-06-03 21:09:05 +0200

    add test d3

---
+++ convert.py
@@ -0,0 +1,15 @@
+import networkx as nx
+import json
+from networkx.readwrite import json_graph
+
+
+
+
+G = nx.barabasi_albert_graph(100, 3)
+data = json_graph.node_link_data(G)
+#write json
+with open('force.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+
+
